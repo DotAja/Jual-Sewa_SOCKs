eb4ffebd3beb062bf39b7708d1378965c1465cdc
DotAja <142836390+DotAja@users.noreply.github.com>
2024-08-18 17:12:57 +0700

    Update 4-ip-egasamp.sh

--- 4-ip-egasamp.sh
+++ 4-ip-egasamp.sh
@@ -5,8 +5,6 @@ echo "==================CREATED BY DOT AJA=================="
 echo "NOTE: masukan alamat ip sesuai urutan dari cloudsigma"
 read -p "Masukkan alamat IP 1: " ip1
 read -p "Masukkan alamat IP 2: " ip2
-read -p "Masukkan alamat IP 3: " ip3
-read -p "Masukkan alamat IP 4: " ip4
 
 sleep 3
 
@@ -24,8 +22,6 @@ clear
 echo "Setup network..."
 
 sudo ip addr add $ip2/24 dev ens4 > /dev/null 2>&1
-sudo ip addr add $ip3/24 dev ens5 > /dev/null 2>&1
-sudo ip addr add $ip4/24 dev ens6 > /dev/null 2>&1
 
 clear
 echo "Network sukses..."
@@ -39,12 +35,6 @@ external: ens3
 
 internal: ens4 port = 1080
 external: ens4
-
-internal: ens5 port = 1080
-external: ens5
-
-internal: ens6 port = 1080
-external: ens6
 
 method: username
 
@@ -81,6 +71,4 @@ clear
 echo "======================PROXY SOCKS====================="
 echo "$ip1:1080:$ngaran:$sentot"
 echo "$ip2:1080:$ngaran:$sentot"
-echo "$ip3:1080:$ngaran:$sentot"
-echo "$ip4:1080:$ngaran:$sentot"
 echo "==================CREATED BY DOT AJA=================="
